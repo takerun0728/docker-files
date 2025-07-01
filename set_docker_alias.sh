2e2b800a29bc4449cc894d6ab38d81eed0b1d55a
Takeru Goto <takeru_goto@jp.honda>
2025-07-01 13:21:32 +0900

    Update quantum

--- set_docker_alias.sh
+++ set_docker_alias.sh
@@ -1,31 +1,46 @@
-alias ndoc_run="docker run --runtime=nvidia \
+alias ndoc_run="docker run --gpus all \
 -it \
 --net=host \
 --cap-add=SYS_PTRACE \
 --privileged \
 -v /run/user:/run/user \
--v $HOME/vscode_remote:/home/docker/.vscode-server \
--v $HOME/.gitconfig:/home/docker/.gitconfig \
--v $HOME/projects:/home/docker/projects \
--v $HOME/bags:/home/docker/bags \
+-v $HOME/vscode_remotee:/home/ubuntu/.vscode-server \
+-v $HOME/.gitconfig:/home/ubuntu/.gitconfig \
+-v $HOME/docker_share:/home/ubuntu/share \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -e DISPLAY=$DISPLAY \
 -e NVIDIA_DRIVER_CAPABILITIES=all"
 
-alias wsldoc_run="docker run --runtime=nvidia \
+alias wsldoc_run="docker run \
 -it \
 --net=host \
 --cap-add=SYS_PTRACE \
 --privileged \
 --device=/dev/dxg:/dev/dxg \
 -v /run/user:/run/user \
--v $HOME/vscode_remote:/home/docker/.vscode-server \
--v $HOME/.gitconfig:/home/docker/.gitconfig \
--v $HOME/projects:/home/docker/projects \
--v $HOME/bags:/home/docker/bags \
+-v $HOME/vscode_remote:/home/ubuntu/.vscode-server \
+-v $HOME/.gitconfig:/home/ubuntu/.gitconfig \
+-v $HOME/docker_share:/home/ubuntu/share \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v /mnt/wslg:/mnt/wslg \
--v /usr/lib/wsl:/usr/lib/wsl \
+-e DISPLAY=$DISPLAY \
+-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+-e XDG_RUNTIME=$XDG_RUNTIME \
+-e PULSE_SERVER=$PULSE_SERVER \
+-e LD_LIBRARY_PATH=/usr/lib/wsl/lib"
+
+alias wslndoc_run="docker run --gpus all \
+-it \
+--net=host \
+--cap-add=SYS_PTRACE \
+--privileged \
+--device=/dev/dxg:/dev/dxg \
+-v /run/user:/run/user \
+-v $HOME/vscode_remote:/home/ubuntu/.vscode-server \
+-v $HOME/.gitconfig:/home/ubuntu/.gitconfig \
+-v $HOME/docker_share:/home/ubuntu/share \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v /mnt/wslg:/mnt/wslg \
 -e DISPLAY=$DISPLAY \
 -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
 -e XDG_RUNTIME=$XDG_RUNTIME \
